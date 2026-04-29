45318bd564577e3bf0f787f024773d15e6fdc327
Peushi Ariyawansa <peushi.ariyawansa@epita.fr>
2026-04-29 10:25:08 +0200

    feat(mongodb): added daily spending aggregation analytics

--- app/services/transaction_service.py
+++ app/services/transaction_service.py
@@ -3,6 +3,7 @@ from datetime import datetime, timezone, timedelta
 from app.db.mongo import transactions_collection
 
 
+# 1. CREATE TRANSACTION
 def create_transaction(data):
     transaction = {
         "user_id": data["user_id"],
@@ -21,31 +22,53 @@ def create_transaction(data):
     }
 
 
+# 2. FRAUD: HIGH FREQUENCY
 def suspicious_transaction_frequency():
     one_minute_ago = datetime.now(timezone.utc) - timedelta(minutes=1)
 
     pipeline = [
         {
             "$match": {
-                "timestamp": {
-                    "$gte": one_minute_ago
-                }
+                "timestamp": {"$gte": one_minute_ago}
             }
         },
         {
             "$group": {
                 "_id": "$user_id",
-                "transaction_count": {
-                    "$sum": 1
-                }
+                "transaction_count": {"$sum": 1}
             }
         },
         {
             "$match": {
-                "transaction_count": {
-                    "$gt": 5
-                }
+                "transaction_count": {"$gt": 5}
             }
+        }
+    ]
+
+    return list(transactions_collection.aggregate(pipeline))
+
+
+
+# 3. ANALYTICS: DAILY SPENDING
+def daily_spending_analysis():
+    pipeline = [
+        {
+            "$group": {
+                "_id": {
+                    "user_id": "$user_id",
+                    "date": {
+                        "$dateToString": {
+                            "format": "%Y-%m-%d",
+                            "date": "$timestamp"
+                        }
+                    }
+                },
+                "total_amount": {"$sum": "$amount"},
+                "transaction_count": {"$sum": 1}
+            }
+        },
+        {
+            "$sort": {"total_amount": -1}
         }
     ]
 
--- tests/test_aggregation.py
+++ tests/test_aggregation.py
@@ -5,3 +5,14 @@ from app.services.transaction_service import (
 results = suspicious_transaction_frequency()
 
 print(results)
+
+from app.services.transaction_service import (
+    daily_spending_analysis
+)
+
+print("\nDaily Spending Analysis:\n")
+
+results = daily_spending_analysis()
+
+for result in results[:5]:
+    print(result)
