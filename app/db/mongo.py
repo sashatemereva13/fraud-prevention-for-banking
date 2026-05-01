726d7ac57d253622e9094202b4d1a31d8d165059
Peushi Ariyawansa <peushi.ariyawansa@epita.fr>
2026-05-01 11:37:53 +0200

    perf(mongodb): added indexes for user and timestamp queries

--- app/db/mongo.py
+++ app/db/mongo.py
@@ -10,3 +10,9 @@ db = client["fraud_detection"]
 transactions_collection = db["transactions"]
 users_collection = db["users"]
 alerts_collection = db["alerts"]
+
+# INDEXES 
+
+transactions_collection.create_index("sender.user_id")
+transactions_collection.create_index("timestamp")
+transactions_collection.create_index("receiver.user_id")
